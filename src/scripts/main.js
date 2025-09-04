38495acf433db69a63c0457caf86d856b65a30e9
TetiankaSh <shh.tetiana@gmail.com>
2025-09-04 16:31:41 +0300

    1 attempt

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+document.addEventListener('click', (ev) => {
+  // Only react if the click is on a <span> inside a <li>
+  if (ev.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const li = ev.target.closest('li');
+
+  if (!li) {
+    return;
+  }
+
+  li.classList.toggle('open');
+});
